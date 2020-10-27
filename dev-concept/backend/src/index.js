d17eed7338e28e36140677cb19c4565bfcd3e56b
Vítor Marco <vitoradrianomarco@gmail.com>
2020-10-27 19:06:58 -0300

    Init backend

---
+++ dev-concept/backend/src/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get('/projects', (request, response) => {
+  const { title, owner } = request.query;
+
+  console.log(title);
+
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
+});
+
+app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+
+  console.log(title, owner);
+
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+
+  console.log(id);
+
+  return response.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
+});
+
+// We need to listen a port
+app.listen(3333, () => {
+  console.log('back-end started!');
+});
