e060aff108ae91722ce59fb9a22840d1b23d036a
Vítor Marco <vitoradrianomarco@gmail.com>
2020-10-29 18:58:05 -0300

    Configuração do babel

---
+++ dev-concept/frontend/babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env', // vai entender o ambiente que a app está sendo usada
+    '@babel/preset-react', // Vai entender o html dentro do js
+  ],
+};
