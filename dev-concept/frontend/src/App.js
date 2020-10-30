362ae6b8f9b5e2955fecf836dee8b4f670d663bf
Vítor Marco <vitoradrianomarco@gmail.com>
2020-10-29 21:10:38 -0300

    Estado e imutabilidade

--- dev-concept/frontend/src/App.js
+++ dev-concept/frontend/src/App.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App () {
+  const [projects, setProjects] = useState(['Desenvolvimento APP', 'Front-end web']);
+
+  const handleAddProject = () => {
+    setProjects([ ...projects, `Novo Project ${Date.now()}`]);
+  }
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects" >
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
+      <Header title="Projects" />
+
+      <ul>
+        { projects.map((project, index) => <li key={`list_${index}`}>{ project }</li>) }
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
   );
 }
