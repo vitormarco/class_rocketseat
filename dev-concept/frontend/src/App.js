c98798a76a9d40470280ff2904d1c604d7afc543
Vítor Marco <vitoradrianomarco@gmail.com>
2020-10-31 11:31:10 -0300

    Listando os projetos do back-end

--- dev-concept/frontend/src/App.js
+++ dev-concept/frontend/src/App.js
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-import api from './service/api';
+import api from './services/api';
 
 import './App.css'
-import backgroundImage from './assets/background.jpg'
 
 import Header from './components/Header';
 
 function App () {
-  const [projects, setProjects] = useState(['Desenvolvimento APP', 'Front-end web']);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects')
+      .then(response => {
+        setProjects(response.data);
+      });
+  }, []);
 
   const handleAddProject = () => {
     setProjects([ ...projects, `Novo Project ${Date.now()}`]);
@@ -18,10 +24,8 @@ function App () {
     <>
       <Header title="Projects" />
 
-      <img width={300} src={backgroundImage} />
-
       <ul>
-        { projects.map((project, index) => <li key={`list_${index}`}>{ project }</li>) }
+        { projects.map(project => <li key={project.id}>{ project.title }</li>) }
       </ul>
 
       <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
