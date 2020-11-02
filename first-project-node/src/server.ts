06cd64becf5a8a02061c803781d4bf8b2422d32a
Vítor Marco <vitoradrianomarco@gmail.com>
2020-11-02 14:00:20 -0300

    Um pouco sobre debug

--- first-project-node/src/server.ts
+++ first-project-node/src/server.ts
@@ -2,9 +2,8 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (request, response) =>
-  response.json({ message: 'Hello GoStack!' }),
-);
+app.use(routes);
 
 app.listen(3333);
