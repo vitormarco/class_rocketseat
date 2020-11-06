c596790a9a072fc3e96e04cc0872560a0c348471
Vítor Marco <vitoradrianomarco@gmail.com>
2020-11-05 21:12:27 -0300

    DTO e Omit

--- first-project-node/src/routes/appointments.routes.ts
+++ first-project-node/src/routes/appointments.routes.ts
@@ -26,7 +26,10 @@ appointmentsRouter.post('/', (request, response) => {
       .status(400)
       .json({ message: 'This appointment is already booked' });
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json(appointment);
 });
