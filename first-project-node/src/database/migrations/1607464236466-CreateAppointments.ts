4492fed452c3d0fa84179904abfc66e5bed728cd
Vítor Marco <vitoradrianomarco@gmail.com>
2020-12-09 11:15:30 -0300

    Alterando implementação do create e a list

--- first-project-node/src/database/migrations/1607464236466-CreateAppointments.ts
+++ first-project-node/src/database/migrations/1607464236466-CreateAppointments.ts
@@ -13,6 +13,7 @@ export default class CreateAppointments1607464236466
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()'
           },
           {
             name: 'provider',
