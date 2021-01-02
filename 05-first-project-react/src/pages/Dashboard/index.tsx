1313463d4e8298f6e5a7950f7a25f5bc6ee76122
Vítor Marco <vitoradrianomarco@gmail.com>
2021-01-02 09:27:40 -0300

    Tela de dashboard com estilos

--- 05-first-project-react/src/pages/Dashboard/index.tsx
+++ 05-first-project-react/src/pages/Dashboard/index.tsx
@@ -1,9 +1,66 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explore" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/31700114?s=460&u=2b889643a8867d50d688453c76aebe18c095d1ce&v=4"
+            alt="Vítor Marco"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              eleifend lacus vel urna varius, eu bibendum nisl commodo.
+            </p>
+          </div>
+          <FiChevronRight size={50} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/31700114?s=460&u=2b889643a8867d50d688453c76aebe18c095d1ce&v=4"
+            alt="Vítor Marco"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              eleifend lacus vel urna varius, eu bibendum nisl commodo.
+            </p>
+          </div>
+          <FiChevronRight size={50} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/31700114?s=460&u=2b889643a8867d50d688453c76aebe18c095d1ce&v=4"
+            alt="Vítor Marco"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              eleifend lacus vel urna varius, eu bibendum nisl commodo.
+            </p>
+          </div>
+          <FiChevronRight size={50} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
