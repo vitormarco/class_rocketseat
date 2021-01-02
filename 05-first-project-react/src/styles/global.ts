c29f8386b45d69b8b35b10f92d7eae2afb6b6fd2
Vítor Marco <vitoradrianomarco@gmail.com>
2021-01-02 08:19:20 -0300

    Adicionando estilo global

--- 05-first-project-react/src/styles/global.ts
+++ 05-first-project-react/src/styles/global.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+import githubBackground from '../assets/github-background.svg';
+
 export default createGlobalStyle`
   * {
     margin: 0;
@@ -9,6 +11,21 @@ export default createGlobalStyle`
   }
 
   body {
-    background: #F2F2FA;
+    background: #F2F2FA url(${githubBackground}) no-repeat 70% top;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 16px Roboto, sans-serif;
+  }
+
+  #root {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+  button {
+    cursor: pointer;
   }
 `;
