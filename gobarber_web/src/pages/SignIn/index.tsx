92ac4917253aefc9d9b828b7f530e39701af634c
Vítor Marco <vitoradrianomarco@gmail.com>
2021-01-03 16:52:05 -0300

    Transformando input e button em componentes

--- gobarber_web/src/pages/SignIn/index.tsx
+++ gobarber_web/src/pages/SignIn/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
 import { Container, Content, Background } from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => (
   <Container>
@@ -12,10 +15,10 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input type="text" name="email" id="email" placeholder="E-mail" />
-        <input type="password" name="password" id="password" placeholder="Senha" />
+        <Input icon={FiMail} type="text" name="email" id="email" placeholder="E-mail" />
+        <Input icon={FiLock} type="password" name="password" id="password" placeholder="Senha" />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
         <a href="forgot">Esqueci minha senha</a>
       </form>
 
