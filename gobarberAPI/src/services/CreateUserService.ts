2035d6b4c64d0bafbbc3e905ae51f35600b21737
Vítor Marco <vitoradrianomarco@gmail.com>
2021-01-03 11:11:50 -0300

    Rename

---
+++ gobarberAPI/src/services/CreateUserService.ts
@@ -0,0 +1,40 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import AppError from '../errors/AppError';
+
+import User from '../models/User';
+
+interface UserRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: UserRequest): Promise<User> {
+    const userRepository = getRepository(User);
+
+    const checkUserExists = await userRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw new AppError('Email already exists.');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = userRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
