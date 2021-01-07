7c0fe4ee4794ebccf862e30641ab95f3bc2aa9c2
Vítor Marco <vitoradrianomarco@gmail.com>
2021-01-07 08:04:57 -0300

    Aplicando rotas que só pode ser acessada se estiver logado

--- gobarber_web/src/routes/index.tsx
+++ gobarber_web/src/routes/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
+import Route from './Route';
 import SigIn from '../pages/SignIn';
 import SigUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SigIn} />
     <Route path="/signup" component={SigUp} />
+
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
