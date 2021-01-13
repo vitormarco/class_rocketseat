a9479f9e1e46bd5a1d0390277828cc972af9a613
Vítor Marco <vitoradrianomarco@gmail.com>
2021-01-13 15:21:40 -0300

    Realizando tdd no SendForgotPasswordEmail

--- gobarberAPI/src/modules/users/services/AuthenticateUserService.spec.ts
+++ gobarberAPI/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -44,7 +44,7 @@ describe('CreateUser', () => {
       fakeHashProvider,
     );
 
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'johndoe@example.com',
         password: '123456',
@@ -72,7 +72,7 @@ describe('CreateUser', () => {
       password: '123456',
     });
 
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'johndoe@example.com',
         password: 'wrong-password',
