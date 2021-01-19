dffb1533ad71e1aece7484f8301b348005235d1b
Vítor Marco <vitoradrianomarco@gmail.com>
2021-01-19 19:46:04 -0300

    Mail Template

--- gobarberAPI/src/shared/container/providers/index.ts
+++ gobarberAPI/src/shared/container/providers/index.ts
@@ -6,12 +6,20 @@ import DiskStorageProvider from './StorageProvider/implementations/DiskStoragePr
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
+import IMailTemplateProvider from './MailTempleteProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './MailTempleteProvider/implementations/HandlebarsMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
 
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
+);
+
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  new EtherealMailProvider(),
+  container.resolve(EtherealMailProvider),
 );
