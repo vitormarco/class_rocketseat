d86580571cfc2b93d766cf42fe28d9c879175e14
Vítor Marco <vitoradrianomarco@gmail.com>
2021-01-22 18:28:28 -0300

    Realizando método de listagem de prestadores de serviços

--- gobarberAPI/src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ gobarberAPI/src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -1,4 +1,4 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Not, Repository } from 'typeorm';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
@@ -22,6 +22,22 @@ class UsersRepository implements IUsersRepository {
     const user = await this.ormRepository.findOne({ where: { email } });
 
     return user;
+  }
+
+  public async findAllProviders(except_user_id?: string): Promise<User[]> {
+    let users: User[];
+
+    if (except_user_id) {
+      users = await this.ormRepository.find({
+        where: {
+          id: Not(except_user_id),
+        },
+      });
+    } else {
+      users = await this.ormRepository.find();
+    }
+
+    return users;
   }
 
   public async create(userData: ICreateUserDTO): Promise<User> {
