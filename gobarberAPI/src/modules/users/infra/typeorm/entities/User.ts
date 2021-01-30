7c2ab4f991dad5435db7f4a3ce518954b7808f3f
Vítor Marco <vitoradrianomarco@gmail.com>
2021-01-30 00:12:38 -0300

    separando as configs de upload e conectando com CDN

--- gobarberAPI/src/modules/users/infra/typeorm/entities/User.ts
+++ gobarberAPI/src/modules/users/infra/typeorm/entities/User.ts
@@ -7,6 +7,8 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
+import uploadConfig from '@config/upload';
+
 import { Exclude, Expose } from 'class-transformer';
 
 @Entity('users')
@@ -35,9 +37,14 @@ class User {
 
   @Expose({ name: 'avatar_url' })
   getAvatarUrl(): string | null {
-    return this.avatar
-      ? `${process.env.APP_API_URL}/files/${this.avatar}`
-      : null;
+    if (!this.avatar) return null;
+
+    switch (uploadConfig.driver) {
+      case 's3':
+        return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.avatar}`;
+      default:
+        return `${process.env.APP_API_URL}/files/${this.avatar}`;
+    }
   }
 }
 
