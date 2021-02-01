2ebefa14d9bd13201c7f30ede671e2c0911f1207
Vítor Marco <vitoradrianomarco@gmail.com>
2021-01-31 23:49:17 -0300

    Refatorando os testes

--- gobarberAPI/src/modules/appointments/services/ListProvidersService.ts
+++ gobarberAPI/src/modules/appointments/services/ListProvidersService.ts
@@ -29,8 +29,6 @@ class ListProvidersService {
         except_user_id: user_id,
       });
 
-      console.log('Query no banco');
-
       await this.cacheProvider.save<User[]>(`providers-list:${user_id}`, users);
     }
 
