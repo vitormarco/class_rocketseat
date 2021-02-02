aef8fdc6ac7764efe7a3acded9ccf219904af6c8
Vítor Marco <vitoradrianomarco@gmail.com>
2021-02-02 11:57:19 -0300

    Clientes dos agendamentos

--- gobarberAPI/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ gobarberAPI/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -19,7 +19,7 @@ class ListProviderAppointmentsService {
 
     @inject('CacheProvider')
     private cacheProvider: ICacheProvider,
-  ) {}
+  ) { }
 
   public async execute({
     provider_id,
@@ -29,9 +29,11 @@ class ListProviderAppointmentsService {
   }: IRequest): Promise<Appointment[]> {
     const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
 
-    let appointments = await this.cacheProvider.recover<Appointment[]>(
-      cacheKey,
-    );
+    // let appointments = await this.cacheProvider.recover<Appointment[]>(
+    //   cacheKey,
+    // );
+
+    let appointments = null;
 
     if (!appointments) {
       appointments = await this.appointmentsRepository.findAllInDayFromProvider(
--- gobarberAPI/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ gobarberAPI/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getDaysInMonth, getDate } from 'date-fns';
+import { getDaysInMonth, getDate, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -19,7 +19,7 @@ class ListProviderMothAvailabilityService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
-  ) {}
+  ) { }
 
   public async execute({
     provider_id,
@@ -38,13 +38,16 @@ class ListProviderMothAvailabilityService {
     );
 
     const availability = eachDayArray.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+
       const appointmentsInDay = appointments.filter(appointment => {
         return getDate(appointment.date) === day;
       });
 
       return {
         day,
-        available: appointmentsInDay.length < 10,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
       };
     });
 
