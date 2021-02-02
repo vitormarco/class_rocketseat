bf21daaf8f7fea67f4aaad54e40d9a741aef48d5
Vítor Marco <vitoradrianomarco@gmail.com>
2021-02-02 18:13:57 -0300

    Retirando informação de password do usuário

--- gobarberAPI/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ gobarberAPI/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -29,11 +30,9 @@ class ListProviderAppointmentsService {
   }: IRequest): Promise<Appointment[]> {
     const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
 
-    // let appointments = await this.cacheProvider.recover<Appointment[]>(
-    //   cacheKey,
-    // );
-
-    let appointments = null;
+    let appointments = await this.cacheProvider.recover<Appointment[]>(
+      cacheKey,
+    );
 
     if (!appointments) {
       appointments = await this.appointmentsRepository.findAllInDayFromProvider(
@@ -45,7 +44,7 @@ class ListProviderAppointmentsService {
         },
       );
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
