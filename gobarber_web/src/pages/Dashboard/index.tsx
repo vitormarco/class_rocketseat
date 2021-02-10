818ab09ae08034e10b62c8e12e7829f660f75c66
Vítor Marco <vitoradrianomarco@gmail.com>
2021-02-10 17:27:18 -0300

    Criando parte de próximo agendamento

--- gobarber_web/src/pages/Dashboard/index.tsx
+++ gobarber_web/src/pages/Dashboard/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
 import {
   Container,
   Header,
   HeaderContent,
   Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
 } from './styles';
 
 import logoImg from '../../assets/logo.svg';
@@ -32,6 +36,32 @@ const Dashboard: React.FC = () => {
         </HeaderContent>
       </Header>
 
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://lh3.googleusercontent.com/ogw/ADGmqu8hAaDeKhmzMfLWsQuxY698UipEFE0Oj8mEB6fMTA=s32-c-mo"
+                alt="Vítor Marco"
+              />
+              <strong>Vítor Marco</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
