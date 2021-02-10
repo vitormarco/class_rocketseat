3547dc534d1dddd998c6e6add8e910553d75ead8
Vítor Marco <vitoradrianomarco@gmail.com>
2021-02-10 17:53:11 -0300

    Listagem de agendamentos

--- gobarber_web/src/pages/Dashboard/index.tsx
+++ gobarber_web/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiClock, FiPower } from 'react-icons/fi';
 
 import {
@@ -9,6 +9,8 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Section,
+  Appointment,
   Calendar,
 } from './styles';
 
@@ -16,6 +18,8 @@ import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -59,6 +63,69 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://lh3.googleusercontent.com/ogw/ADGmqu8hAaDeKhmzMfLWsQuxY698UipEFE0Oj8mEB6fMTA=s32-c-mo"
+                  alt="Vítor Marco"
+                />
+                <strong>Vítor Marco</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://lh3.googleusercontent.com/ogw/ADGmqu8hAaDeKhmzMfLWsQuxY698UipEFE0Oj8mEB6fMTA=s32-c-mo"
+                  alt="Vítor Marco"
+                />
+                <strong>Vítor Marco</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://lh3.googleusercontent.com/ogw/ADGmqu8hAaDeKhmzMfLWsQuxY698UipEFE0Oj8mEB6fMTA=s32-c-mo"
+                  alt="Vítor Marco"
+                />
+                <strong>Vítor Marco</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://lh3.googleusercontent.com/ogw/ADGmqu8hAaDeKhmzMfLWsQuxY698UipEFE0Oj8mEB6fMTA=s32-c-mo"
+                  alt="Vítor Marco"
+                />
+                <strong>Vítor Marco</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
