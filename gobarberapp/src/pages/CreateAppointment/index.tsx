376b8dc788fc953d0e28901cc4893af56a8dc7e2
Vítor Marco <vitoradrianomarco@gmail.com>
2021-02-16 00:57:53 -0300

    Criando um agendamento

--- gobarberapp/src/pages/CreateAppointment/index.tsx
+++ gobarberapp/src/pages/CreateAppointment/index.tsx
@@ -29,6 +29,8 @@ import {
   SectionContent,
   Hour,
   HourText,
+  CreateAppointmentButton,
+  CreateAppointmentButtonText,
 } from './styles';
 
 import { Provider } from '../Dashboard';
@@ -46,7 +48,7 @@ interface AvailabilityItem {
 const CreateAppointment: React.FC = () => {
   const { user } = useAuth();
   const route = useRoute();
-  const { goBack } = useNavigation();
+  const { goBack, navigate } = useNavigation();
 
   const { providerId } = useMemo(() => route.params as RouteParams, [
     route.params,
@@ -101,6 +103,28 @@ const CreateAppointment: React.FC = () => {
   const handleSelectHour = useCallback((hour: number) => {
     setSelectedHour(hour);
   }, []);
+
+  const handleCreateAppointment = useCallback(async () => {
+    try {
+      const date = new Date(selectedDate);
+
+      date.setHours(selectedHour);
+      date.setMinutes(0);
+
+      await api.post('appointments', {
+        provider_id: selectedProvider,
+        date,
+      });
+
+      navigate('ApointmentCreated', { date: date.getTime() });
+    } catch (err) {
+      Alert.alert(
+        'Ocorreu um erro ao cadastrar um agendamento',
+        err?.response?.data?.message ||
+          'Ocorreu um erro ao tentar criar um agendamento, tente novamente',
+      );
+    }
+  }, [navigate, selectedDate, selectedHour, selectedProvider]);
 
   const getProvidersList = useCallback(async () => {
     try {
@@ -245,6 +269,9 @@ const CreateAppointment: React.FC = () => {
             </SectionContent>
           </Section>
         </Schedule>
+        <CreateAppointmentButton onPress={handleCreateAppointment}>
+          <CreateAppointmentButtonText>Agendar</CreateAppointmentButtonText>
+        </CreateAppointmentButton>
       </Content>
     </Container>
   );
